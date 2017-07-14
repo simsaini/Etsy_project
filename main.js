416fbbc4472f80620210d60f46b1befeac66151f
Simran Saini <Simranstory101@gmail.com>
2017-07-14 09:54:40 -0400

    final project

---
+++ main.js
@@ -0,0 +1,75 @@
+// Since the `data.js` file is loaded in your `index.html` before this one,
+// you have access to the `data` variable that contains the Etsy Store data.
+// Open your `index.html` page in your browser and you can verify that the following
+// is properly working. The `data` variable is an array with 25 items in it
+console.log(data);
+
+
+
+// 1: Show me how to calculate the average price of all items.
+function question1() {
+  let sum = 0;
+for (let i = 0; i < data.length; i++){
+  sum += data[i].price;
+}
+console.log(sum / data.length);
+}
+
+
+// 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
+function question2() {
+  let newArray = [];
+  for (let i = 0; i < data.length; i++){
+    if (data[i].price <= 18 && data[i].price >= 14) {
+      newArray.push(data[i].title);
+    }
+  }
+  console.log(newArray);
+}
+
+
+
+// 3: Which item has a "GBP" currency code? Display it's name and price.
+function question3 () {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].currency_code === "GBP") {
+      console.log(data[i].title + " cost " + data[i].price);
+    }
+  }
+}
+// console.log(question3());
+
+
+// 4: Display a list of all items who are made of wood.
+function question4 () {
+  let array = [];
+  for (let i = 0; i < data.length; i++) {
+  if (/wood/i.test(data[i].materials)){
+    array.push(data[i].title)
+  }
+  }
+  console.log(array);
+}
+
+// 5: Which items are made of eight or more materials?
+//    Display the name, number of items and the items it is made of.
+function question5 () {
+  let array2 = []
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].materials.length >= 8) {
+       array2.push(data[i].title + " has " + data[i].materials.length + " materials");
+       array2.push(data[i].materials);
+    }
+  }
+  console.log(array2);
+}
+
+// 6: How many items were made by their sellers?
+// Answer:
+function question6 () {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].who_made === "i_did"){
+      console.log( "items were made by their sellers");
+    }
+  }
+}
